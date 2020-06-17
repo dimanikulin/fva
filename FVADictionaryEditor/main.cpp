15cf50bbd16e5bd70e066a0a516935eb6672534c
dimanikulin <dimanikulin@gmail.com>
2020-06-17 22:51:57 +0300

    input parameters update

--- FVADictionaryEditor/main.cpp
+++ FVADictionaryEditor/main.cpp
@@ -1,6 +1,7 @@
 #include "fvadictionaryeditor.h"
 #include <QtGui/QApplication>
 #include "fvacommonlib.h"
+#include <qtextcodec>
 
 int main(int argc, char *argv[])
 {
@@ -10,8 +11,13 @@ int main(int argc, char *argv[])
 	{
 		return FVA_ERROR_NOT_ENOUGH_ARG;
 	}
-
-	FVADictionaryEditor w(a.arguments().at(1));
+	QString deviceMatchName;
+	if ( a.arguments().size() >= 3 )
+	{
+		deviceMatchName = a.arguments().at(2);
+	}
+	QTextCodec::setCodecForTr(QTextCodec::codecForName("Windows-1251"));
+	FVADictionaryEditor w(a.arguments().at(1), deviceMatchName);
 	w.show();
 	return a.exec();
 }
