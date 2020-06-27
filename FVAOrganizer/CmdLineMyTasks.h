00bd941e5206ef0e59f8234d266e7d8fe0973537
dimanikulin <dimanikulin@gmail.com>
2020-06-27 14:22:19 +0300

    added include guard

--- FVAOrganizer/CmdLineMyTasks.h
+++ FVAOrganizer/CmdLineMyTasks.h
@@ -1,7 +1,11 @@
+#ifndef _CMD_LINE_MY_TASKS_H_
+#define _CMD_LINE_MY_TASKS_H_
+
 #include "CmdLineTasks.h"
 
 _CLASS_TASK_DECLARATION(CLT_Xml_Convert,false)
 _CLASS_TASK_DECLARATION(CLT_Update_File_Description,false)
 _CLASS_TASK_DECLARATION(CLT_Convert_Dir_Desc,true)
 _CLASS_TASK_DECLARATION(CLT_Rename_File_By_Desc,false)
-_CLASS_TASK_DECLARATION(CLT_Desc_To_SQL,false)
+
+#endif // _CMD_LINE_MY_TASKS_H_
