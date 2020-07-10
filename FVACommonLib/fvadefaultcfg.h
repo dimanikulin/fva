848a31ac570f78ede7711e9e794bef9bb2a8e59d
dimanikulin <dimanikulin@gmail.com>
2020-07-10 09:58:30 +0300

    #21 added header to keep all the cgf constant

---
+++ FVACommonLib/fvadefaultcfg.h
@@ -0,0 +1,12 @@
+#ifndef FVADEFAULTCFG_H
+#define FVADEFAULTCFG_H
+
+const QString	FVA_DB_NAME							= "digikam4.db";
+const QString	FVA_DEFAULT_ROOT_DIR				= "C:/FVA/";
+
+/*!
+ * \brief how many supported files should be in a folder
+ */
+const unsigned int FVA_DEFAULT_MIN_COUNT_FILES_IN_DIR	= 2;
+
+#endif // FVADEFAULTCFG_H
