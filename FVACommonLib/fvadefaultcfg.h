e14de0f5389a156d8bd6b1368c2b06c82977d205
dimanikulin <dimanikulin@gmail.com>
2020-07-10 10:44:28 +0300

    #21 added date file system name formats as cfg constants

--- FVACommonLib/fvadefaultcfg.h
+++ FVACommonLib/fvadefaultcfg.h
@@ -9,4 +9,20 @@ const QString	FVA_DEFAULT_ROOT_DIR				= "C:/FVA/";
  */
 const unsigned int FVA_DEFAULT_MIN_COUNT_FILES_IN_DIR	= 2;
 
+
+/*!
+ * \brief format a dir name as a date
+ */
+const QString FVA_DIR_NAME_FMT = "yyyy.MM.dd";
+
+/*!
+ * \brief format a dir name as a year
+ */
+const QString FVA_DIR_NAME_YEAR_FMT = "yyy";
+
+/*!
+ * \brief format a file name as a date + time
+ */
+const QString FVA_FILE_NAME_FMT = "yyyy-MM-dd-hh-mm-ss";
+
 #endif // FVADEFAULTCFG_H
