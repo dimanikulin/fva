e7a48839b9abb45e3f4a951632ee9d8926f74366
dimanikulin <dimanikulin@gmail.com>
2020-07-20 10:21:57 +0300

    #35 added fvaconstants.h to keep constants/enums/typedefs

--- FVACommonLib/fvaconstants.h
+++ FVACommonLib/fvaconstants.h
@@ -1,4 +1,39 @@
 #ifndef FVACONSTS_H
 #define FVACONSTS_H
 
+#include <QString>
+#include <QMap>
+
+/*!
+ * \brief it enumerates internal valuable file system types
+ */
+enum FVA_FS_TYPE
+{
+	FVA_FS_TYPE_UNKNOWN = 0,
+	FVA_FS_TYPE_IMG		= 1,
+	FVA_FS_TYPE_VIDEO	= 2,
+	FVA_FS_TYPE_AUDIO	= 3,
+
+	FVA_FS_TYPE_DIR		= 4,
+};
+
+/*!
+ * \brief internal file names
+ */
+const QString	FVA_DESCRIPTION_FILE_NAME			= "description.csv";
+const QString	FVA_DIR_DESCRIPTION_FILE_NAME		= "folderDescription.json";
+const QString	FVA_BACKGROUND_MUSIC_FILE_NAME		= "bgmusic.mid";
+
+const QString	FVA_TARGET_FOLDER_NAME				= "TARGET_FOLDER_NAME"; 
+
+/*!
+ * \brief id for any object that is not defined
+ */
+const int FVA_UNDEFINED_ID	= -1;
+
+class fvaDevice;
+typedef QMap<int, fvaDevice> DEVICE_MAP;
+
+typedef QMap< QString, QStringList > DESCRIPTIONS_MAP;
+
 #endif // FVACONSTS_H
