195812f7ff43f691ffb823f9c1f74956bc8f22a9
dimanikulin <dimanikulin@gmail.com>
2020-07-29 22:01:14 +0300

    #44 refactored binary structure and installation

--- FVAScripts/fixCLT2Issues.py
+++ FVAScripts/fixCLT2Issues.py
@@ -57,7 +57,7 @@ with open(sys.argv[1], newline='') as csvfile:
         if row[0] == sys.argv[2] and row[0] == 'FVA_ERROR_NO_DEV_ID':
             print(', '.join(row))
 
-        if row[0] == sys.argv[2] and row[0] == 'FVA_ERROR_EMPTY_DEVICE' and row[2] == '85':
+        if row[0] == sys.argv[2] and row[0] == 'FVA_ERROR_EMPTY_DEVICE' and row[2] == '15':
             print(', '.join(row))
             os.chmod(row[1], stat.S_IWRITE) # clear read only file attribute
             subprocess.call(['../jhead.exe', "-te", "source.JPG" , row[1]])
