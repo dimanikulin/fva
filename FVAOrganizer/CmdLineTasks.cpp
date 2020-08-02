120fbfa711aa8b13a1f8b7ba30f4003742bdeaec
dimanikulin <dimanikulin@gmail.com>
2020-08-02 10:20:43 +0300

    #53 moved implementation of CLT_Folder_Merging to old cmds

--- FVAOrganizer/CmdLineTasks.cpp
+++ FVAOrganizer/CmdLineTasks.cpp
@@ -585,77 +585,6 @@ FVA_ERROR_CODE CLT_Convert_Amr::execute()
 	}
 	return FVA_NO_ERROR;
 }
-FVA_ERROR_CODE CLT_Folder_Merging::execute()
-{
-	QString subFolder	= m_folder;
-	subFolder.remove(m_baseFolder);
-
-	if (!m_dir.exists(m_custom + subFolder + QDir::separator()))
-	{
-		// skip internal folder 
-		if (!subFolder.contains("#copy"))
-		{
-			if (!m_dir.mkpath(m_custom + subFolder + QDir::separator()))
-			{
-				LOG_QCRIT << "could not create dest folder:" << m_custom + subFolder + QDir::separator();
-				return FVA_ERROR_CANT_CREATE_DIR;
-			}
-		}
-	}
-	else
-	{
-		if (!subFolder.isEmpty())
-		{
-			if (m_dir.exists(m_custom + subFolder + " #1" + QDir::separator() ))
-			{
-				if( !m_dir.rename( m_custom + subFolder + " #1" + QDir::separator(), m_custom + subFolder + " #2" + QDir::separator() ) )
-					LOG_QWARN << "could not rename source :" << m_custom + subFolder + " #1" + QDir::separator() << " into " << m_custom + subFolder + " #2" + QDir::separator();
-				else
-					LOG_QWARN << "renamed source :" << m_custom + subFolder + " #1/" << " into " << m_custom + subFolder + " #2";
-			}
-
-			if( !m_dir.rename( m_custom + subFolder, m_custom + subFolder + " #1" + QDir::separator() ) )
-				LOG_QWARN << "could not rename source :" << m_custom + subFolder << " into " << m_custom + subFolder + " #1/";
-			else
-				LOG_QWARN << "renamed source :" << m_custom + subFolder << " into " << m_custom + subFolder + " #1/";
-			
-			// skip internal folder 
-			if (!subFolder.contains("#copy"))	
-				m_dir.mkpath(m_custom + subFolder + QDir::separator());
-		}
-	}
-
-	// create folder structure the same as in source folder
-	Q_FOREACH(QFileInfo info, m_dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst))
-	{				
-		QString original	= m_folder  + QDir::separator() + info.fileName();
-		QString dest		= m_custom + subFolder + QDir::separator() + info.fileName();
-
-		// skip internal folder 
-		if (original.contains("#copy") || dest.contains("#copy"))
-			continue;	
-		
-		if( !m_dir.rename( original, dest ) )
-		{			
-			if(QDir(original).entryInfoList(QDir::NoDotAndDotDot|QDir::AllEntries).count() == 0)
-			{
-				// empty folder now - no need in it to keep
-				if (!m_dir.rmdir(original))
-				{
-					LOG_QCRIT << "could not remove empty source:" << original;
-					return FVA_ERROR_CANT_MOVE_DIR;
-				}
-				else
-					continue;
-			}
-			
-			LOG_QCRIT << "could not move:" << original << " into " << dest;
-			return FVA_ERROR_CANT_MOVE_DIR;
-		}
-		LOG_QDEB << "merged:" << original << " into " << dest;
-	}
-	return FVA_NO_ERROR;
-}
 
 FVA_ERROR_CODE CLT_Set_File_Atts::execute()
 {
@@ -687,7 +616,7 @@ FVA_ERROR_CODE CLT_Set_File_Atts::execute()
 	
 	return FVA_NO_ERROR;
 }
-FVA_ERROR_CODE CLT_One_Event_Folder_Merging::execute()
+FVA_ERROR_CODE CLT_1_Event_Folder_Merging::execute()
 {
 	Q_FOREACH(QFileInfo info, m_dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst))
 	{				
