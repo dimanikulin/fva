4959adf138cf18524a5d28275b02c45283486077
dimanikulin <dimanikulin@gmail.com>
2020-08-05 10:34:19 +0300

    #3 fvaGetExifMakeAndModelFromFile and fvaGetExifDateTimeOriginalFromFile now are implemented

--- FVACommonLib/fvacommonexif.cpp
+++ FVACommonLib/fvacommonexif.cpp
@@ -1,1 +1,14 @@
 #include "fvacommonexif.h"
+
+#include "qexifimageheader.h"
+
+QString fvaGetExifMakeAndModelFromFile(const QString& pathToFile)
+{
+	return QExifImageHeader(pathToFile).value(QExifImageHeader::Make).toString()
+	+ QExifImageHeader(pathToFile).value(QExifImageHeader::Model).toString();
+}
+
+QDateTime fvaGetExifDateTimeOriginalFromFile(const QString& pathToFile)
+{
+	return QExifImageHeader().value(QExifImageHeader::DateTimeOriginal).toDateTime();
+}
--- FVACommonLib/fvacommonexif.h
+++ FVACommonLib/fvacommonexif.h
@@ -1,5 +1,22 @@
 #ifndef FVACOMMONEXIF_H
 #define FVACOMMONEXIF_H
 
+#include <QtCore/QString>
+#include <QtCore/QDateTime>
+
+/*!
+* \brief it returns Exif camera maker plus model string
+* \param pathToFile path file to load camera maker plus model from
+* \returns it returns empty string if loading of camera maker and model was not successful, 
+* otherwize it returns string that contains <CAMERA MAKER>+<CAMERA MODEL>
+*/
+QString fvaGetExifMakeAndModelFromFile(const QString& pathToFile);
+
+/*!
+* \brief it returns Exif DateTimeOriginal attribute converted to QDateTime
+* \param pathToFile path file to load DateTimeOriginal from
+* \returns it returns empty QDateTime if loading of DateTimeOriginal not successful, otherwize it returns DateTimeOriginal attribute converted to QDateTime
+*/
+QDateTime fvaGetExifDateTimeOriginalFromFile(const QString& pathToFile);
 
 #endif // FVACOMMONEXIF_H
