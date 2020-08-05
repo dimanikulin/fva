38ca924b5f73e1cb5b2ac6e6001886f264419853
dimanikulin <dimanikulin@gmail.com>
2020-08-05 09:30:16 +0300

    #58 disabled moving alone files to folder up

--- FVACommonLib/fvadefaultcfg.h
+++ FVACommonLib/fvadefaultcfg.h
@@ -7,7 +7,7 @@ const QString	FVA_DEFAULT_ROOT_DIR				= "C:/FVA/";
 /*!
  * \brief how many supported files should be in a folder
  */
-const unsigned int FVA_DEFAULT_MIN_COUNT_FILES_IN_DIR	= 2;
+const unsigned int FVA_DEFAULT_MIN_COUNT_FILES_IN_DIR	= 0;
 
 /*!
  * \brief format a dir name as a date
