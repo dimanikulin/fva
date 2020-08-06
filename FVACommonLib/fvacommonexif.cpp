f6f6c93ebbf2d4e69effb0516a318e1278baa90a
dimanikulin <dimanikulin@gmail.com>
2020-08-06 09:26:40 +0300

    #3 reworked to use easyexif but not qexifimageheader

--- FVACommonLib/fvacommonexif.cpp
+++ FVACommonLib/fvacommonexif.cpp
@@ -1,14 +1,31 @@
 #include "fvacommonexif.h"
+#include "fvadefaultcfg.h"
 
-#include "qexifimageheader.h"
+#include "../easyexif/exif.h"
+#include <QtCore\QFile>
 
 QString fvaGetExifMakeAndModelFromFile(const QString& pathToFile)
 {
-	return QExifImageHeader(pathToFile).value(QExifImageHeader::Make).toString()
-	+ QExifImageHeader(pathToFile).value(QExifImageHeader::Model).toString();
+	QFile file(pathToFile);
+	if (file.open(QIODevice::ReadOnly)){
+		QByteArray data = file.readAll(/*1024*1024*/);
+		easyexif::EXIFInfo info;
+		if (0 == info.parseFrom((unsigned char *)data.data(), data.size()) ){
+			return  QString(info.Make.c_str()) + info.Model.c_str();
+		}
+	}
+	return "";
 }
 
 QDateTime fvaGetExifDateTimeOriginalFromFile(const QString& pathToFile)
 {
-	return QExifImageHeader(pathToFile).value(QExifImageHeader::DateTimeOriginal).toDateTime();
+	QFile file(pathToFile);
+	if (file.open(QIODevice::ReadOnly)){
+		QByteArray data = file.readAll(/*1024 * 100*/);
+		easyexif::EXIFInfo info;
+		if (0== info.parseFrom((unsigned char *)data.data(), data.size())){
+			return  QDateTime::fromString(info.DateTimeOriginal.c_str(), EXIF_DATE_TIME_FMT);
+		}
+	}
+	return QDateTime();
 }
