15b7796b7ca3dee81f3dc0640d71feed198c32a1
dimanikulin <dimanikulin@gmail.com>
2020-08-06 09:18:29 +0300

    #3 added EXIF_DATE_TIME_FMT to use

--- FVACommonLib/fvadefaultcfg.h
+++ FVACommonLib/fvadefaultcfg.h
@@ -20,8 +20,13 @@ const QString FVA_DIR_NAME_FMT = "yyyy.MM.dd";
 const QString FVA_DIR_NAME_YEAR_FMT = "yyyy";
 
 /*!
- * \brief format a file name as a date + time
+ * \brief format a FVA file name as a date + time
  */
 const QString FVA_FILE_NAME_FMT = "yyyy-MM-dd-hh-mm-ss";
+
+/*!
+* \brief format an EXIF date + time
+*/
+const QString EXIF_DATE_TIME_FMT = "yyyy:MM:dd hh:mm:ss";
 
 #endif // FVADEFAULTCFG_H
