e43d1c62c96997a0a7c9a1e00eca808fa387ab13
dimanikulin <dimanikulin@gmail.com>
2020-08-07 08:38:55 +0300

    #60 removed all sql files with data as it was moved to fva/#data#/*.csv

--- FVASQL/06.deviceTypes.sql
+++
@@ -1,5 +0,0 @@
-BEGIN TRANSACTION;
-insert into fvaDeviceTypes values (0,"N/A");
-insert into fvaDeviceTypes values (1,"Photo/video device");
-insert into fvaDeviceTypes values (2,"Scaner");
-COMMIT;
--- FVASQL/08.params.sql
+++
@@ -1,7 +0,0 @@
-BEGIN TRANSACTION;
-insert into fvaParams values (1 ,"Version", "0.27");
-insert into fvaParams values (2 ,"LastUpdated", "06/28/2020");
-insert into fvaParams values (3 ,"isSearchByDateTimeEnabled", "TRUE");
-insert into fvaParams values (4 ,"isSearchByLocationEnabled", "TRUE");
-
-COMMIT;
