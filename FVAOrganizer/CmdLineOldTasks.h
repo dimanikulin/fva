f166914be9dcc7bb50ac80409b7976ae6c54f754
dimanikulin <dimanikulin@gmail.com>
2020-08-28 21:38:37 +0300

    #74 moved CLTCreateDirStructByDeviceName to separate files

--- FVAOrganizer/CmdLineOldTasks.h
+++ FVAOrganizer/CmdLineOldTasks.h
@@ -3,11 +3,6 @@
 
 #include "CmdLineBaseTask.h"
 
-/*!
- * \brief it creates folder structure using device names in input folder
- */
-_CLASS_TASK_DECLARATION(CLT_Dir_Struct_Create_By_Device_Name,false)
-
 _CLASS_TASK_DECLARATION(CLT_Files_Rename_By_Dir,false)
 
 /*!
