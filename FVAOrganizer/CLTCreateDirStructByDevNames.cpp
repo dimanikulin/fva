a5d9c5effd29d7f9c8bae4e60ef376c4be348996
dimanikulin <dimanikulin@gmail.com>
2020-08-28 21:36:47 +0300

    #74 created separated files for CLTCreateDirStructByDeviceName

---
+++ FVAOrganizer/CLTCreateDirStructByDevNames.cpp
@@ -0,0 +1,67 @@
+
+#include "CLTCreateDirStructByDevNames.h"
+#include "fvacommonexif.h"
+
+FVA_EXIT_CODE CLTCreateDirStructByDeviceName::execute()
+{
+	QString deviceName;
+	Q_FOREACH(QFileInfo info, m_dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden | QDir::AllDirs | QDir::Files, QDir::DirsFirst))
+	{
+		if (info.isDir())
+			continue;
+		// if it is picture files
+		QString suffix = info.suffix().toUpper();
+		QString fullname = info.absoluteFilePath();
+		if (FVA_FS_TYPE_IMG == fvaConvertFileExt2FileType(suffix))
+		{
+			QString newDeviceName = fvaGetExifMakeAndModelFromFile(info.filePath());
+
+			while (newDeviceName.endsWith('\n'))
+				newDeviceName.chop(1);
+
+			while (newDeviceName.endsWith('\r'))
+				newDeviceName.chop(1);
+
+			while (newDeviceName.endsWith(QChar('\0')))
+				newDeviceName.chop(1);
+
+			newDeviceName = newDeviceName.trimmed();
+
+			QString dirName;
+			if (newDeviceName.isEmpty())
+			{
+				LOG_QDEB << "no device name in picture:" << info.absoluteFilePath();
+				dirName = "EMPTY";
+			}
+			else
+			{
+				if (deviceName.isEmpty())
+					LOG_QDEB << "got first device name: " << newDeviceName << "in picture:" << info.absoluteFilePath();
+				else if (deviceName != newDeviceName)
+					LOG_QWARN << "got new device name: " << newDeviceName << "in picture:" << info.absoluteFilePath() << "old: " << deviceName;
+
+				deviceName = newDeviceName;
+				dirName = deviceName;
+			}
+
+			QString fullSubFolderpath = m_folder + "/" + dirName;
+
+			if (!m_dir.exists(dirName))
+			{
+				m_dir.mkdir(dirName);
+				LOG_QDEB << "sub-folder:" << dirName << " created";
+			}
+			// move the file
+			if (!m_dir.rename(info.absoluteFilePath(), fullSubFolderpath + "/" + info.fileName()))
+			{
+				LOG_QCRIT << "can not rename file:" << info.absoluteFilePath() << " into:" << fullSubFolderpath + "/" + info.fileName();
+				return FVA_ERROR_CANT_RENAME_FILE;
+			}
+			else
+				LOG_QDEB << "file renamed:" << info.absoluteFilePath() << " into:" << fullSubFolderpath + "/" + info.fileName();
+		}
+		else
+			LOG_QDEB << " no device name in:" << info.absoluteFilePath();
+	}
+	return FVA_NO_ERROR;
+}
---
+++ FVAOrganizer/CLTCreateDirStructByDevNames.h
@@ -0,0 +1,23 @@
+#ifndef _CLT_CREATE_DIR_STRUCTURE_BY_DEVICE_NAMES_H_
+#define _CLT_CREATE_DIR_STRUCTURE_BY_DEVICE_NAMES_H_
+
+#include "CmdLineBaseTask.h"
+
+/*!
+ * \brief it creates folder structure using device names in input folder
+ */
+class CLTCreateDirStructByDeviceName : public CmdLineBaseTask
+{
+public:
+	/// CmdLineBaseTask interface
+	CLTCreateDirStructByDeviceName(const QString& dir_, bool readOnly_ = false, const QString& custom_ = "")
+	:CmdLineBaseTask(dir_, readOnly_, custom_)
+									{	LOG_QDEB << "cmd created, dir:" << dir_;		}
+	virtual ~CLTCreateDirStructByDeviceName()	{ LOG_QDEB << "cmd deleted, dir:" << m_folder; }
+	virtual FVA_EXIT_CODE execute();
+	static QString Name()			{	return "CLTCreateDirStructByDeviceName";						}
+	virtual bool supportReadOnly()	{	return false;									}
+
+}; 
+
+#endif // _CLT_CREATE_DIR_STRUCTURE_BY_DEVICE_NAMES_H_
