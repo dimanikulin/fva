4db83b17aee5426c0325e1ebe976062040ee9867
dimanikulin <dimanikulin@gmail.com>
2020-08-29 12:45:26 +0300

    #61 added writing input foders into fvaFolder.csv

--- FVAOrganizer/CmdLineCSVTasks.cpp
+++ FVAOrganizer/CmdLineCSVTasks.cpp
@@ -7,16 +7,27 @@ FVA_EXIT_CODE CLT_Fva_Folder_2_CSV::execute()
 	int ID = FVA_UNDEFINED_ID;
 	FVA_EXIT_CODE res = fvaGetIDFromFile(FVA_DEFAULT_ROOT_DIR +"#data#/fvaFolder.id", ID);
 	RET_RES_IF_RES_IS_ERROR
-	QString csvRecord =  QString::number(++ID) + "," // ID
-		+ FVA_TARGET_FOLDER_NAME		+ "," // Name	
-		+ m_custom						// DevId
-		+ ",,,,,,,,";	//Tags,People,PlaceId,EventId,ReasonPeople,LinkedFolder,WhoTookFotoId,Scanerid
+	QFile fileNew(FVA_DEFAULT_ROOT_DIR + "#data#/fvaFolderN.csv");
+	if (!fileNew.open(QIODevice::WriteOnly | QIODevice::Text))
+		return FVA_ERROR_CANT_OPEN_NEW_DIR_DESC;
+	QTextStream writeStream(&fileNew);
 
-	QFile fileNew ( FVA_DEFAULT_ROOT_DIR + "#data#/fvaFolderN.csv" );		
-	if ( !fileNew.open( QIODevice::WriteOnly | QIODevice::Text ) )	
-		return FVA_ERROR_CANT_OPEN_NEW_DIR_DESC;	
-	QTextStream writeStream( &fileNew );	
-	writeStream << csvRecord;	
+	Q_FOREACH(QFileInfo info, m_dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden | QDir::AllDirs | QDir::Files, QDir::DirsFirst))
+	{
+		// just skip internal folder
+		if ((info.isDir() && info.fileName()[0] == '#' && info.fileName()[info.fileName().size() - 1] == '#')
+			||
+			info.isFile())
+			continue;
+		QString dir = info.absoluteFilePath().replace("\\", "/");  // replace slaches on backslashes
+		dir = dir.remove(FVA_DEFAULT_ROOT_DIR); // remove a prefix as root dir
+		QString csvRecord = QString::number(++ID) + "," // ID
+			+ dir + "," // Name	
+			+ m_custom						// DevId
+			+ ",,,,,,,,";	//Tags,People,PlaceId,EventId,ReasonPeople,LinkedFolder,WhoTookFotoId,Scanerid
+		writeStream << csvRecord;
+	}
+		
 	writeStream.flush();	
 	fileNew.close();	
 	return fvaSaveIDInFile(FVA_DEFAULT_ROOT_DIR +"#data#/fvaFolder.id", ID);
