1e5037208ebb490ee92484000f6fa5470233f3db
dimanikulin <dimanikulin@gmail.com>
2020-08-30 20:55:30 +0300

    #68 fixed cmd name

--- FVAOrganizerWizard/FVAOrganizerPages.cpp
+++ FVAOrganizerWizard/FVAOrganizerPages.cpp
@@ -313,8 +313,8 @@ bool FVAOrganizerDevicePage::validatePage()
 	IF_CLT_ERROR_SHOW_MSG_BOX_AND_RET_FALSE("CLTRenameFiles RO mode")
 	exitCode = fvaRunCLT("CLTRenameFiles", ((FVAOrganizerWizard*)wizard())->inputFolder());
 	IF_CLT_ERROR_SHOW_MSG_BOX_AND_RET_FALSE("CLTRenameFiles")
-	exitCode = fvaRunCLT("CLT_Fva_Files_2_CSV", ((FVAOrganizerWizard*)wizard())->inputFolder(), true, false, QString::number(deviceId));
-	IF_CLT_ERROR_SHOW_MSG_BOX_AND_RET_FALSE("CLT_Fva_Files_2_CSV")
+	exitCode = fvaRunCLT("CLTCSVFvaFile", ((FVAOrganizerWizard*)wizard())->inputFolder(), true, false, QString::number(deviceId));
+	IF_CLT_ERROR_SHOW_MSG_BOX_AND_RET_FALSE("CLTCSVFvaFile")
 	exitCode = fvaRunCLT("CLT_Dir_Struct_Create_By_File", ((FVAOrganizerWizard*)wizard())->inputFolder(), true, false, QString::number(deviceId));
 	IF_CLT_ERROR_SHOW_MSG_BOX_AND_RET_FALSE("CLT_Dir_Struct_Create_By_File")
 	exitCode = fvaRunCLT("CLT_Alone_Files_Move", ((FVAOrganizerWizard*)wizard())->inputFolder());
