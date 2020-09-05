268f8d9e8d2a14fbc37f2f00d2f04ba4ef79cef2
dimanikulin <dimanikulin@gmail.com>
2020-09-05 09:41:00 +0300

    #68 moved CLTJSONCreateDirStructByFileNames to separated files

--- FVAOrganizer/CmdLineOldTasks.h
+++ FVAOrganizer/CmdLineOldTasks.h
@@ -4,11 +4,6 @@
 #include "CmdLineBaseTask.h"
 
 /*!
- * \brief it creates folder structure by days folders, using file names in input folder - old impl
- */
-_CLASS_TASK_DECLARATION(CLT_Dir_Struct_Create_By_File_Old,true)
-
-/*!
  * \brief it moves alone (or twins) files to parent folder, with adding their description into parent folder DESCRIPTION_FILE_NAME - old impl
  */
 _CLASS_TASK_DECLARATION(CLT_Alone_Files_Move_Old,true)
