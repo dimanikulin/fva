c42a688a00432d3c25057749ee3582204631c312
dimanikulin <dimanikulin@gmail.com>
2020-10-24 14:28:57 +0300

    #78 added empty implementation for CLTCSVUpdateFvaFiles

--- FVAOrganizer/CLTCSVUpdateFvaFiles.cpp
+++ FVAOrganizer/CLTCSVUpdateFvaFiles.cpp
@@ -1,1 +1,8 @@
-ddd
+#include "CLTCSVUpdateFvaFiles.h"
+
+#include "FVADescriptionFile.h"
+
+FVA_EXIT_CODE CLTCSVUpdateFvaFiles::execute()
+{
+	return FVA_NO_ERROR;
+}
--- FVAOrganizer/CLTCSVUpdateFvaFiles.h
+++ FVAOrganizer/CLTCSVUpdateFvaFiles.h
@@ -1,24 +1,22 @@
-#ifndef _CMD_CSV_FVA_FILES_H_
-#define _CMD_CSV_FVA_FILES_H_
+#ifndef _CMD_CSV_UPDATE_FVA_FILES_H_
+#define _CMD_CSV_UPDATE_FVA_FILES_H_
 
 #include "CmdLineBaseTask.h"
 
-/*!
- * \brief it creates CSV file with FVA information for files in a folder
- */
-class CLTCSVFvaFile : public CmdLineBaseTask
+
+class CLTCSVUpdateFvaFiles : public CmdLineBaseTask
 {
 public:
 	/// CmdLineBaseTask interface
-	CLTCSVFvaFile(const QString& dir_, bool readOnly_ = false, const QString& custom_ = "")
+	CLTCSVUpdateFvaFiles(const QString& dir_, bool readOnly_ = false, const QString& custom_ = "")
 		:CmdLineBaseTask(dir_, readOnly_, custom_)
 	{
 		LOG_QDEB << "cmd created,dir:" << dir_ << ",RO=" << (readOnly_ ? "yes" : "no") << ",SRO=" << (supportReadOnly() ? "yes" : "no");
 	}
-	virtual ~CLTCSVFvaFile()		{ LOG_QDEB << "cmd deleted, dir:" << m_folder; }
+	virtual ~CLTCSVUpdateFvaFiles()		{ LOG_QDEB << "cmd deleted, dir:" << m_folder; }
 	virtual FVA_EXIT_CODE execute();
-	static QString Name()			{ return "CLTCSVFvaFile"; }
+	static QString Name()			{ return "CLTCSVUpdateFvaFiles"; }
 	virtual bool supportReadOnly()	{ return true; }
 };
 
-#endif // _CMD_CSV_FVA_FILES_H_
+#endif // _CMD_CSV_UPDATE_FVA_FILES_H_
