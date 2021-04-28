1ead79d447b2732533025c2f25df8d08879e8c08
dimanikulin <dimanikulin@gmail.com>
2021-04-28 09:44:24 +0300

    #95 organizerWizard and viewer cleaned up

--- FVAOrganizerWizard/main.cpp
+++ FVAOrganizerWizard/main.cpp
@@ -5,7 +5,6 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	// QTextCodec::setCodecForTr(QTextCodec::codecForName("Windows-1251"));
 	FVAOrganizerWizard w;
 	w.show();
 	return a.exec();
