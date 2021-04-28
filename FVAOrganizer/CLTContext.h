e8948a329ba6f1d1508c1156c31ae2964007a39a
dimanikulin <dimanikulin@gmail.com>
2021-04-28 19:30:37 +0300

    #98 created CLTContext to keep CLT config and each command parameters

---
+++ FVAOrganizer/CLTContext.h
@@ -0,0 +1,74 @@
+#ifndef CLT_CONTEXT_H_
+#define CLT_CONTEXT_H_
+
+#include <QtCore/QString>
+
+
+/*!
+ * \brief to keep command line task context (+settings) that is known before task is started
+ */
+class CLTContext
+{
+	// data 
+	public:
+
+		////////////////////////// ONE COMMAND CONFIGURATION  //////////////////////////////////////////
+		/*!
+		 * \brief path to work folder of command
+		 */
+		QString dir;
+
+		/*!
+		 * \brief does command work in read-only mode
+		 */
+		bool readOnly;
+
+		/*!
+		* \brief a parameter to customize a command  - might be string or integer inside
+		*/
+		QString custom;
+
+		////////////////////////// COMMON CONFIGURATION  //////////////////////////////////////////
+		/*!
+		 * \brief path to FVA root folder
+		 */
+		QString	rootDir;
+
+		/*!
+		 * \brief how many supported files should be in a folder
+		 */
+		unsigned int minCountFilesInADir;
+
+		////////////////////////// RENAMING FILE OPTIONS   //////////////////////////////////////////
+		/*!
+		 * \brief shall be renamed pictures files using the file modification time if they do not have exif taken time set
+		 */
+		bool renamePicsByModificationTimeIfEmptyExif;
+
+		/*!
+		 * \brief shall be renamed video files using the file modification time if they do not have taken time set
+		 */
+		bool renameVideoByModificationTimeIfEmptyExif;
+
+		////////////////////////// FORMATTING STRING OPTIONS/////////////////////////////////////////
+		/*!
+		 * \brief format a dir name as a date
+		 */
+		QString dirNameFmt;
+
+		/*!
+		 * \brief format a dir name as a year
+		 */
+		QString dirNameYearFmt;
+
+		/*!
+		 * \brief format a FVA file name as a date + time
+		 */
+		QString fileNameFmt;
+
+		/*!
+		 * \brief format an EXIF date + time
+		 */
+		QString ExidDateTimeFmt;
+
+#endif // CLT_CONTEXT_H_
