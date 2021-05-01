16b444c45bdad76862a9054ac262c25fb2439d26
dimanikulin <dimanikulin@gmail.com>
2021-05-01 18:47:42 +0300

    #104 added fvadataprocessor skeleton file

---
+++ FVADataProcessor/fvadataprocessor_global.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <QtCore/qglobal.h>
+
+#ifndef BUILD_STATIC
+# if defined(FVADATAPROCESSOR_LIB)
+#  define FVADATAPROCESSOR_EXPORT Q_DECL_EXPORT
+# else
+#  define FVADATAPROCESSOR_EXPORT Q_DECL_IMPORT
+# endif
+#else
+# define FVADATAPROCESSOR_EXPORT
+#endif
