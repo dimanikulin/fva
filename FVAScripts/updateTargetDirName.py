25a73321b06cf6325f437d4c213d91ea76f95cb5
dimanikulin <dimanikulin@gmail.com>
2021-05-01 18:42:28 +0300

    #96 deleted updateTargetDirName.py

--- FVAScripts/updateTargetDirName.py
+++
@@ -1,6 +0,0 @@
-import sys
-import fileinput
-
-with fileinput.FileInput(sys.argv[1], inplace=True, backup='.bak') as file:
-    for line in file:
-        print(line.replace('TARGET_FOLDER_NAME', sys.argv[2]), end='')
