a7556d7a0b65bde5a29bc6521294ac39a3bc860c
dimanikulin <dimanikulin@gmail.com>
2021-05-03 20:56:03 +0300

    #108 refactored to have separate function for each check parameter

--- FVAFlowController/FVAFlowController.cpp
+++ FVAFlowController/FVAFlowController.cpp
@@ -10,69 +10,77 @@
 #include "fvacommoncsv.h"
 #include "fvaconstants.h"
 
+FVA_EXIT_CODE performDeviceChecks(const QString& dir, DeviceContext& deviceContext, const QString& rootSWdir)
+{
+	FVA_EXIT_CODE exitCode = fvaRunCLT("CLTCheckDeviceName", dir);
+	if (FVA_ERROR_NON_UNIQUE_DEVICE_NAME == exitCode)
+	{
+		exitCode = fvaRunCLT("CLTCreateDirStructByDeviceName", dir);
+		FVA_MESSAGE_BOX("Found several devices in a folder, please select other dir!");
+		return exitCode;
+	}
+	else IF_CLT_ERROR_SHOW_MSG_BOX_AND_RET_FALSE("CLTCheckDeviceName")
+
+	exitCode = fvaLoadDeviceMapFromCsv(rootSWdir, deviceContext.fullDeviceMap);
+	IF_ERROR_SHOW_MSG_BOX_AND_RET_EXITCODE("fvaLoadDeviceMapFromCsv")
+
+	PEOPLE_MAP peopleMap;
+	exitCode = fvaLoadPeopleMapFromCsv(rootSWdir, peopleMap);
+	IF_ERROR_SHOW_MSG_BOX_AND_RET_EXITCODE("fvaLoadPeopleMapFromCsv")
+
+	for (auto it = deviceContext.fullDeviceMap.begin(); it != deviceContext.fullDeviceMap.end(); ++it)
+		it.value().ownerName = peopleMap[it.value().ownerId].name;
+
+	QDir _dir(dir);
+	Q_FOREACH(QFileInfo info, _dir.entryInfoList(QDir::System | QDir::Hidden | QDir::Files, QDir::DirsLast))
+	{
+		if (info.isDir())
+			continue;
+		QString suffix = info.suffix().toUpper();
+		FVA_FS_TYPE type = fvaConvertFileExt2FileType(suffix);
+		if (FVA_FS_TYPE_IMG != type)
+			continue;
+
+		deviceContext.deviceMap = fvaGetDeviceMapForImg(deviceContext.fullDeviceMap, info.filePath(), deviceContext.matchedDeviceName);
+		if (!deviceContext.matchedDeviceName.isEmpty())
+			break;
+	}
+	return FVA_NO_ERROR;
+}
+void performOrientationChecks(const QString& dir, QObject* obj)
+{
+	// to run change orentation in auto mode
+	QProcess myProcess(obj);
+	myProcess.setProcessChannelMode(QProcess::MergedChannels);
+	myProcess.start(QCoreApplication::applicationDirPath() + "/jpegr/jpegr.exe -auto " + dir);
+	myProcess.waitForFinished(-1);
+}
+
 FVA_EXIT_CODE FVAFlowController::PerformChecksForInputDir(const QString& dir, DeviceContext& deviceContext, QObject* obj)
 {
 	FvaConfiguration cfg;
 	FVA_EXIT_CODE exitCode = cfg.load(QCoreApplication::applicationDirPath() + "/fvaParams.csv");
 	IF_ERROR_SHOW_MSG_BOX_AND_RET_EXITCODE("cfg.load")
 
+	QString rootSWdir;
+	exitCode = cfg.getParamAsString("Common::RootDir", rootSWdir);
+	IF_ERROR_SHOW_MSG_BOX_AND_RET_EXITCODE("getParamAsString(Common::RootDir)")
+
 	// do we need to search by device?
 	bool SearchByDevice = false;
 	exitCode = cfg.getParamAsBoolean("Search::Device", SearchByDevice);
 	IF_ERROR_SHOW_MSG_BOX_AND_RET_EXITCODE("getParamAsBoolean(Search::Device)")
 
-	QString rootSWdir;
-	exitCode = cfg.getParamAsString("Common::RootDir", rootSWdir);
-	IF_ERROR_SHOW_MSG_BOX_AND_RET_EXITCODE("getParamAsString(Common::RootDir)")
-
 	if (SearchByDevice)
 	{
-		exitCode = fvaRunCLT("CLTCheckDeviceName", dir);
-		if (FVA_ERROR_NON_UNIQUE_DEVICE_NAME == exitCode)
-		{
-			exitCode = fvaRunCLT("CLTCreateDirStructByDeviceName", dir);
-			FVA_MESSAGE_BOX("Found several devices in a folder, please select other dir!");
-			return exitCode;
-		}
-		else IF_CLT_ERROR_SHOW_MSG_BOX_AND_RET_FALSE("CLTCheckDeviceName")
-
-		exitCode = fvaLoadDeviceMapFromCsv(rootSWdir, deviceContext.fullDeviceMap);
-		IF_ERROR_SHOW_MSG_BOX_AND_RET_EXITCODE("fvaLoadDeviceMapFromCsv")
-
-		PEOPLE_MAP peopleMap;
-		exitCode = fvaLoadPeopleMapFromCsv(rootSWdir, peopleMap);
-		IF_ERROR_SHOW_MSG_BOX_AND_RET_EXITCODE("fvaLoadPeopleMapFromCsv")
-
-		for (auto it = deviceContext.fullDeviceMap.begin(); it != deviceContext.fullDeviceMap.end(); ++it)
-			it.value().ownerName = peopleMap[it.value().ownerId].name;
-
-		QDir _dir(dir);
-		Q_FOREACH(QFileInfo info, _dir.entryInfoList(QDir::System | QDir::Hidden | QDir::Files, QDir::DirsLast))
-		{
-			if (info.isDir())
-				continue;
-			QString suffix = info.suffix().toUpper();
-			FVA_FS_TYPE type = fvaConvertFileExt2FileType(suffix);
-			if (FVA_FS_TYPE_IMG != type)
-				continue;
-
-			deviceContext.deviceMap = fvaGetDeviceMapForImg(deviceContext.fullDeviceMap, info.filePath(), deviceContext.matchedDeviceName);
-			if (!deviceContext.matchedDeviceName.isEmpty())
-				break;
-		}
-	} // if (SearchByDevice)
-
-	bool needCheckOrientation;
+		FVA_EXIT_CODE res = performDeviceChecks(dir, deviceContext, rootSWdir);
+		RET_RES_IF_RES_IS_ERROR
+	}
+	bool needCheckOrientation = false;
 	exitCode = cfg.getParamAsBoolean("Common::CheckOrientation", needCheckOrientation);
 	IF_ERROR_SHOW_MSG_BOX_AND_RET_EXITCODE("getParamAsBoolean with Common::CheckOrientation")
 
 	if (needCheckOrientation)
-	{
-		// to run change orentation in auto mode
-		QProcess myProcess(obj);
-		myProcess.setProcessChannelMode(QProcess::MergedChannels);
-		myProcess.start(QCoreApplication::applicationDirPath() + "/jpegr/jpegr.exe -auto " + dir);
-		myProcess.waitForFinished(-1);
-	}
+		performOrientationChecks(dir,obj);
 	return FVA_NO_ERROR;
 }
