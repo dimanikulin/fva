128fbe19f798736547a58db7bee78b79e99a460b
dimanikulin <dimanikulin@gmail.com>
2021-05-03 16:33:49 +0300

    #108 created flow controller

---
+++ FVAFlowController/FVAFlowController.h
@@ -0,0 +1,14 @@
+#ifndef _FVA_FLOW_CONTROLLER_H_
+#define _FVA_FLOW_CONTROLLER_H_
+
+/*
+ * \brief it main class to keep a logic of the operation flow and interaction with a user 
+ */
+class FVAFlowController
+{
+public:
+	FVAFlowController(){};
+	virtual ~FVAFlowController(){};
+};
+#endif
+
