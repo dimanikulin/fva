727659693a15f8c7eb0cafa19bb555cf2f1aa4e6
dimanikulin <dimanikulin@gmail.com>
2021-05-04 13:38:07 +0300

    #104 FVADataProcessor::createTaskByName implemented

--- FVADataProcessor/FVADataProcessor.cpp
+++ FVADataProcessor/FVADataProcessor.cpp
@@ -1,1 +1,23 @@
 #include "FVADataProcessor.h"
+
+#include <QtCore/qdebug>
+#include <QtCore/qdir>
+
+#include "CLTRenameVideoBySequence.h"
+#include "CLTCreateDirStructByDevNames.h"
+#include "CLTConvertAmr.h"
+#include "CLTCheckDeviceName.h"
+#include "CLTAutoChecks1.h"
+
+std::auto_ptr<CmdLineBaseTask> FVADataProcessor::createTaskByName(const CLTContext& context, const FvaConfiguration& cfg)
+{
+	std::auto_ptr<CmdLineBaseTask> r(0);
+
+	if (context.cmdType == CLTCreateDirStructByDeviceName::Name())r.reset(new CLTCreateDirStructByDeviceName());
+	else if (context.cmdType == CLTCheckDeviceName::Name())			r.reset(new CLTCheckDeviceName());
+	else if (context.cmdType == CLTRenameVideoBySequence::Name())	r.reset(new CLTRenameVideoBySequence(cfg));
+	else if (context.cmdType == CLTConvertAmr::Name())				r.reset(new CLTConvertAmr());
+	else if (context.cmdType == CLTAutoChecks1::Name())				r.reset(new CLTAutoChecks1(cfg));
+
+	return r;
+}
--- FVAOrganizer/CmdLineExecutor.cpp
+++ FVAOrganizer/CmdLineExecutor.cpp
@@ -4,8 +4,6 @@
 #include <QtCore/qdir>
 
 #include "CLTRenameFiles.h"
-// #include "CLTRenameVideoBySequence.h"
-// #include "CLTCreateDirStructByDevNames.h"
 #include "CLTFixDuplicatedFileNames.h"
 #include "CLTPrintFSStructure.h"
 
@@ -13,12 +11,9 @@
 #include "CLTMerge1DayEventDir.h"
 #include "CLTMerge1EventDir.h"
 #include "CLTSetFileAtts.h"
-// #include "CLTConvertAmr.h"
 #include "CLTMoveAloneFiles.h"
-//#include "CLTCheckDeviceName.h"
 #include "CLTCreateDirStructByFileNames.h"
 
-// #include "CLTAutoChecks1.h"
 #include "CLTAutoChecks2.h"
 #include "CLTAutoChecks3.h"
 
@@ -29,14 +24,9 @@ std::auto_ptr<CmdLineBaseTask> CmdLineExecutor::createTaskByName(const CLTContex
 	std::auto_ptr<CmdLineBaseTask> r(0);
 	
 	if (context.cmdType == CLTCreateDirStructByFileNames::Name())	r.reset(new CLTCreateDirStructByFileNames());
-	//else if (context.cmdType == CLTCreateDirStructByDeviceName::Name())r.reset(new CLTCreateDirStructByDeviceName());
 	else if (context.cmdType == CLTRenameFiles::Name())				r.reset(new CLTRenameFiles(cfg));
-	// else if (context.cmdType == CLTCheckDeviceName::Name())			r.reset(new CLTCheckDeviceName());
-	//else if (context.cmdType == CLTRenameVideoBySequence::Name())	r.reset(new CLTRenameVideoBySequence(cfg));
 	else if (context.cmdType == CLTAutoChecks2::Name())				r.reset(new CLTAutoChecks2(cfg));
 	else if (context.cmdType == CLTMoveAloneFiles::Name())			r.reset(new CLTMoveAloneFiles(cfg));
-	//else if (context.cmdType == CLTConvertAmr::Name())				r.reset(new CLTConvertAmr());
-	//else if (context.cmdType == CLTAutoChecks1::Name())				r.reset(new CLTAutoChecks1(cfg));
 	else if (context.cmdType == CLTSetFileAtts::Name())				r.reset(new CLTSetFileAtts());
 	else if (context.cmdType == CLTPrintFSStructure::Name())		r.reset(new CLTPrintFSStructure(cfg));
 	else if (context.cmdType == CLTMerge1EventDir::Name())			r.reset(new CLTMerge1EventDir());
