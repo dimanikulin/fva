8986e8662a4c456de41431df18cd83e6d7cef4e7
dimanikulin <dimanikulin@gmail.com>
2021-05-18 09:09:17 +0300

    #119 added python implementation of CLTFixEmptyDateTime

---
+++ FVAScripts/CLTFixEmptyDateTime.py
@@ -0,0 +1,23 @@
+from exif import Image
+import os
+import sys
+import datetime
+
+for filename in os.listdir(sys.argv[1]):
+    fullPath = sys.argv[1] + "/" + filename
+    fiximage = Image(fullPath)
+    if fiximage.has_exif:
+        print(filename + " is good")
+        print(f"{fiximage.datetime_original}\n")
+    else:
+        print(filename + " is NOT good")
+        with open(fullPath, "rb") as fixfile:
+            time = datetime.datetime.fromtimestamp(os.path.getmtime(fullPath))
+            print(time)
+            fiximage.datetime_original = str(time)
+            fiximage.subsec_time_original = '000'
+
+        with open(fullPath, 'wb') as fixedfile:
+           fixedfile.write(fiximage.get_file())
+
+  
