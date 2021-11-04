19e873e4cf17f48e573c2dd1bb453de7d5c5eb7e
dimanikulin <dimanikulin@gmail.com>
2021-11-04 09:03:05 +0200

    #126 deleted as not wanted

--- FVAScripts/updateFvaBinFromRelease.cmd
+++
@@ -1,4 +0,0 @@
-xcopy "C:\git\fva\FVAbin\x64\Release\FVAConfigurator.exe" "C:\FVA\#bin#\FVAConfigurator.exe" /Y /Q
-xcopy "C:\git\fva\FVAbin\x64\Release\FVAOrganizer.exe" "C:\FVA\#bin#\\FVAOrganizer.exe" /Y /Q
-xcopy "C:\git\fva\FVAbin\x64\Release\FVAOrganizerWizard.exe" "C:\FVA\#bin#\FVAOrganizerWizard.exe" /Y /Q
-xcopy "C:\git\fva\FVAbin\x64\Release\FVAViewer.exe" "C:\FVA\#bin#\FVAViewer.exe" /Y /Q  
