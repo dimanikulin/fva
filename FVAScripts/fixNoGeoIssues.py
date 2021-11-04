02f975b8b8bb2bcc2b4f7d901bee247bb92a61c7
dimanikulin <dimanikulin@gmail.com>
2021-11-04 09:47:29 +0200

    #126 added comments for fixNoGeoIssues.py

--- FVAScripts/fixNoGeoIssues.py
+++ FVAScripts/fixNoGeoIssues.py
@@ -5,22 +5,24 @@ import subprocess
 import os, stat
 import win32com.client
 
-#python fixNoGeoIssues.py 1 >> fixNoGeoIssuesPlaceID-1.txt 
-#../exiftool(-k).exe -all= -tagsfromfile @ -all:all -unsafe -icc_profile C:/FVA/2004/2004.06.05-07/2004-06-05-##-##-00.jpg
-#../exiftool(-k).exe -GPSAltitude='0.0' -GPSLatitudeRef=N -GPSLongitudeRef=E -GPSAltitudeRef=0 "C:\FVA\2001\2001.08.20\2001-08-20-##-##-02.JPG"  
-
-#C:\FVANOGEO\Oksana\2007.08.22-30\219/2007-08-25-13-07-59.jpg,14,219,44.57113903709,34.3399139274
-
+# we open the issues csv
 with open("issues3.csv", newline='', encoding='utf-8') as csvfile:
+    # we take line by line from there 
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
+        # if error is set to FVA_ERROR_NO_GEO and placeid in issues csv equals placeid passed as input param  
         if  row[0] == 'FVA_ERROR_NO_GEO' and row[2] == sys.argv[1] :
+            # we go to fvaPlaces.csv 
             with open("../../#data#/fvaPlaces.csv", newline='', encoding='utf-8') as csvfile:
+                # we take line by line from there
                 placereader = csv.reader(csvfile, delimiter=',', quotechar='|')         
                 for placerow in placereader:
+                    # if place id from fvaPlaces.csv equals placeid passed as input param  
                     if placerow[0] == sys.argv[1]:
                         os.chmod(row[1], stat.S_IWRITE) # clear read only file attribute
-                        print(row[1] + ',' + placerow[4] + ',' + placerow[5])
+                        print(row[1] + ',' + placerow[4] + ',' + placerow[5]) # save to log
+                        # update GPS coordinates in a file in FS using  exiftool(-k).exe 
                         p = subprocess.call(['../exiftool(-k).exe','-GPSLatitude=' + placerow[4], '-GPSLongitude=' + placerow[5],'-GPSLatitudeRef=' + placerow[6],'-GPSLongitudeRef=' + placerow[7],'-GPSAltitude=0 -GPSAltitudeRef=0', row[1]])
                         shell = win32com.client.Dispatch("WScript.Shell")
+                        # press Enter to go to next file
                         shell.SendKeys("{ENTER}", 0) 
