790cf6f474f45bb47e4f37aaa5afdaf7cadeb511
dimanikulin <dimanikulin@gmail.com>
2021-12-16 17:52:29 +0200

    #132 added event info page

---
+++ FVAOrganizerWizard/FVAOrganizerEventInfoPage.cpp
@@ -0,0 +1,84 @@
+/*!
+* \file FVAOrganizerEventInfoPage.cpp
+* \copyright Copyright 2021 FVA Software. All rights reserved. This file is released under the XXX License.
+* \author Dima Nikulin.
+* \version 0.29
+* \date  2014-2021
+*/
+#include "FVAOrganizerEventInfoPage.h"
+
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QTreeWidget>
+
+#include <QUrl>
+#include <QDesktopServices>
+
+#include "fvaorganizerwizard.h"
+#include "fvacommonui.h"
+#include "fvalogger.inl"
+#include "FVAFlowController.h"
+
+FVAOrganizerEventInfoPage::FVAOrganizerEventInfoPage(void)
+{
+        LOG_DEB << "FVAOrganizerEventInfoPage construction" ;
+	
+#ifdef  FVA_LANGUAGE_RUS
+	words	= new QLabel(tr("Пожалуйста, заполните информацию по каждой папке(событию),\n  (помеченной красным цветом) \n типом события и причастными людьми"));
+	inputDirButton = new QPushButton;
+	inputDirButton->setText(tr("Открыть папку"));
+	QLabel* dirLabel = new QLabel(tr("Папки (события):"));
+#else 
+#ifdef  FVA_LANGUAGE_ENG
+	words = new QLabel(tr("Please fill the information up for each folder (an event),\n    (marked by red)   \n  by type and related people"));
+	inputDirButton = new QPushButton;
+	inputDirButton->setText(tr("Open a folder"));
+	QLabel* dirLabel = new QLabel(tr("Folders(events):"));
+#endif // FVA_LANGUAGE_ENG
+#endif // FVA_LANGUAGE_RUS
+			
+	QGridLayout * dirLayout	= new QGridLayout;
+	dirLayout->addWidget(words,0,0);
+	dirLayout->addWidget(inputDirButton,1,0);
+
+	QVBoxLayout * layout	= new QVBoxLayout;
+
+	layout->addLayout(dirLayout);
+	layout->addWidget(dirLabel);
+	layout->addWidget(inputDirs);
+
+	QGridLayout * infoLayout= new QGridLayout;
+	dirLayout->addWidget(events,0,0);
+	dirLayout->addWidget(people,0,1);
+
+	logOutput		= new QTextBrowser;
+	layout->addWidget(logOutput);
+
+	setLayout(layout);
+
+	connect( inputDirButton, SIGNAL( clicked() ), this, SLOT( OnFvaInputDirButtonPressed() ) );
+        LOG_DEB << "FVAOrganizerEventInfoPage constructed" ;
+
+}
+void FVAOrganizerEventInfoPage::OnFvaInputDirButtonPressed()
+{
+	QString path = QDir::toNativeSeparators(((FVAOrganizerWizard*)wizard())->inputFolder());
+	LOG_DEB << "FVAOrganizerEventInfoPage::OnFvaInputDirButtonPressed() input dir=" << path;
+	if(!QDesktopServices::openUrl(QUrl::fromLocalFile(((FVAOrganizerWizard*)wizard())->inputFolder())))
+		LOG_DEB << "FVAOrganizerEventInfoPage::OnFvaInputDirButtonPressed() failed to show input dir=" << path;
+	else
+		LOG_DEB << "FVAOrganizerEventInfoPage::OnFvaInputDirButtonPressed() shows input dir=" << path;	
+}
+
+bool FVAOrganizerEventInfoPage::validatePage()
+{
+        LOG_DEB << "FVAOrganizerEventInfoPage validate page" ;
+	FVAFlowController flow;
+	FVA_EXIT_CODE exitCode = flow.ProcessInputDirForEvent(((FVAOrganizerWizard*)wizard())->inputFolder(), "TODO"/*, this*/);
+	if (exitCode != FVA_NO_ERROR)
+		return false;
+
+	return true;
+}
---
+++ FVAOrganizerWizard/FVAOrganizerEventInfoPage.h
@@ -0,0 +1,87 @@
+/*!
+* \file FVAOrganizerEventInfoPage.h
+* \copyright Copyright 2021 FVA Software. All rights reserved. This file is released under the XXX License.
+* \author Dima Nikulin.
+* \version 0.29
+* \date  2014-2021
+*/
+#ifndef _FVA_ORG_EVENT_INFO_H_
+#define _FVA_ORG_EVENT_INFO_H_
+
+#include <QtWidgets/QWizardPage>
+
+class QPushButton;
+class QLabel;
+class QTextBrowser;
+class QTreeWidget;
+
+/*!
+* \brief FVAOrganizerEventInfoPage is a child of <a href="https://doc.qt.io/qt-5/qwizardpage.html">QWizardPage</a> and implements the next UI functions:
+*
+* 1. "Explanation words" to user in a QLabel;
+* 2. The events(aka dirs) we need to add information about, in form of a QTreeWidget;
+* 3. "Open dir" button (QPushButton) to open input dir where processed files are in to recall their content.
+* 4. a QTreeWidget to set up Event Type
+* 5. a QTreeWidget to set up the people list because this event happened of
+* 6. QTextBrowser to output the logging events;
+* This class implements "View" functions from MVC pattern.
+*/
+
+class FVAOrganizerEventInfoPage : public QWizardPage
+{
+	Q_OBJECT
+	public:
+
+		FVAOrganizerEventInfoPage();
+
+	protected:
+
+		/*!
+		* \brief calls a ProcessInputDirForEventInfo of class FVAFlowController
+		* \return it returns nothing
+		*/
+		virtual bool validatePage();
+
+	private slots:
+
+		/*!
+		* \brief to open input dir where processed files are in
+		* \return it returns nothing
+		*/
+		void OnFvaInputDirButtonPressed();
+
+	private:
+	
+		/*!
+		* \brief to tell a user what to do here
+		*/
+		QLabel*		words;
+
+		/*!
+		* \brief to open input dir where processed files are in
+		*/
+		QPushButton*	inputDirButton;
+
+		/*!
+		* \brief to output the logging events
+		*/
+		QTextBrowser*	logOutput;
+		
+		/*!
+		* \brief to output the input dirs structure 
+		*/
+		QTreeWidget*	inputDirs;			
+
+		/*!
+		* \brief to output the structure of people caused the event 
+		*/
+		QTreeWidget*	people;
+
+		/*!
+		* \brief to output the event structure 
+		*/
+		QTreeWidget*	events;	
+			
+};
+
+#endif // _FVA_ORG_EVENT_INFO_H_
