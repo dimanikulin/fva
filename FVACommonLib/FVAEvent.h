f6e72a5ab3d50147f41d36fa8c731b812a95951c
dimanikulin <dimanikulin@gmail.com>
2022-02-20 15:43:44 +0200

    #141 created FVAEvent.h to keep event data

---
+++ FVACommonLib/FVAEvent.h
@@ -0,0 +1,46 @@
+/*!
+ * \file FVAEvent.h
+ * \copyright Copyright 2021 FVA Software. All rights reserved. This file is released under the XXX License.
+ * \author Dima Nikulin.
+ * \version 0.29
+ * \date  2014-2022
+ */
+#ifndef FVAEVENT_H
+#define FVAEVENT_H
+
+#include <QtCore/QString>
+#include <QtCore/QMap>
+
+#include "fvaconstants.h"
+
+/*!
+ * \brief it describes whole information about event
+ ID,Name,Type,Institution
+ */
+class fvaEvent
+{
+	public:
+		/*
+		 * event identifier from event dictionary - fvaEvents.csv
+		 */
+		int ID;
+
+		/*
+		 * name 
+		 */
+		QString name;
+
+		/*
+		 * relation type from fvaRelationTypes.csv
+		 */
+		unsigned int type;
+
+		/*
+		 * institution type from fvaInstitutions.csv
+		 */
+		unsigned int Institution;
+};
+
+typedef QMap<int, fvaEvent> EVENT_MAP;
+
+#endif // FVAEVENT_H
