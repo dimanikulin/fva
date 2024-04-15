63edbd07e807f82eaf71c4e279f2c5b9d8110fae
Dmytro Nikulin <dimanikulin@gmail.com>
2024-04-15 09:06:32 +0300

    #150 fix in include

--- FVACommonLib/test/test_CmdLineBaseTask.cpp
+++ FVACommonLib/test/test_CmdLineBaseTask.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 // #include <gmock/gmock.h>
-#include "CmdLineBaseTask.h"
+#include "../CmdLineBaseTask.h"
 
 /*
 // Mock implementation of CmdLineBaseTask for testing purposes
