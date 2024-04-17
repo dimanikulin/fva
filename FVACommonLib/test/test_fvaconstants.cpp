58ef1d936ca1cde347717683e64a937bf98f0b6c
Dmytro Nikulin <dimanikulin@gmail.com>
2024-04-17 08:52:12 +0300

    #150 added test fvaconstants

---
+++ FVACommonLib/test/test_fvaconstants.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include "../fvaconstants.h"
+
+// Test case to verify the value of FVA_BACKGROUND_MUSIC_FILE_NAME
+TEST(FVAConstantsTests, BackgroundMusicFileNameTest)
+{
+    // Assert
+    EXPECT_EQ(FVA_BACKGROUND_MUSIC_FILE_NAME, "bgmusic.mid");
+}
