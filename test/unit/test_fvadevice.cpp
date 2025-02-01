c55ca85049cc478c454e8f77a1a8d4b759dff4d0
Dmytro Nikulin <dimanikulin@gmail.com>
2025-02-01 13:46:39 +0200

    #168 moved to test folder

--- test/unit/test_fvadevice.cpp
+++ test/unit/test_fvadevice.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "../fvadevice.h"
+#include "fvadevice.h"
 
 // Test case for verifying the initialization of fvaDevice object
 TEST(fvaDeviceTests, InitializationTest)
