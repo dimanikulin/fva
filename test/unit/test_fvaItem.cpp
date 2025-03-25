d96c8589dffebd829f22348a7e0d6efa8a09a1f5
Dmytro Nikulin <dimanikulin@gmail.com>
2025-03-25 11:02:31 +0200

    #150 fixed build

--- test/unit/test_fvaItem.cpp
+++ test/unit/test_fvaItem.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 #include "fvaItem.h"
+№include "fvaFolder.h"
+#include "fvaFile.h"
 
 // Test case for FVA_FS_TYPE_DIR with valid dateTo and pFvaFolder
 TEST(FvaItemTests, GetGuiName_Directory_ValidDateToAndFolder)
