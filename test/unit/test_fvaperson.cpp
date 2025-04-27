9ec4e5d718a2332598ed85a4a61486342cb8ae8d
Dmytro Nikulin <dimanikulin@gmail.com>
2025-04-27 10:41:47 +0300

    #150 added person TC

--- test/unit/test_fvaperson.cpp
+++ test/unit/test_fvaperson.cpp
@@ -37,28 +37,3 @@ TEST_F(FVAPersonTests, Constructor)
     EXPECT_EQ(person.type, type);
     EXPECT_EQ(person.institution, institution);
 }
-
-TEST_F(FVAPersonTests, GettersAndSetters)
-{
-    // Arrange
-    fvaPerson person;
-    int id = 1;
-    QString name = "John";
-    QString fullName = "John Doe";
-    int type = 2;
-    int institution = 3;
-
-    // Act
-    person.setId(id);
-    person.setName(name);
-    person.setFullName(fullName);
-    person.setType(type);
-    person.setInstitution(institution);
-
-    // Assert
-    EXPECT_EQ(person.getId(), id);
-    EXPECT_EQ(person.getName(), name);
-    EXPECT_EQ(person.getFullName(), fullName);
-    EXPECT_EQ(person.getType(), type);
-    EXPECT_EQ(person.getInstitution(), institution);
-}
