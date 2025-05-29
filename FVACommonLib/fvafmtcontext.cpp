7c3f47359c8d50337aa6afbb6e7973a3afd749f5
Dmytro Nikulin <dimanikulin@gmail.com>
2025-05-29 10:15:29 +0300

    #150 added logging

--- FVACommonLib/fvafmtcontext.cpp
+++ FVACommonLib/fvafmtcontext.cpp
@@ -11,25 +11,58 @@
 FVA_EXIT_CODE FvaFmtContext::fillFmtContextFromCfg(const FvaConfiguration& cfg)
 {
 	FVA_EXIT_CODE res = cfg.getParamAsString("Format::fvaDirName", fvaDirName);
-	RET_RES_IF_RES_IS_ERROR
-
+	if ( FVA_NO_ERROR != res )
+	{
+		LOG_CRIT << "FvaFmtContext::fillFmtContextFromCfg: Can't get parameter 'Format::fvaDirName' from configuration.";
+		return res;
+	}
+	
 	res = cfg.getParamAsString("Format::fvaDirNameYear", fvaDirNameYear);
-	RET_RES_IF_RES_IS_ERROR
-
+	if ( FVA_NO_ERROR != res )
+	{
+		LOG_CRIT << "FvaFmtContext::fillFmtContextFromCfg: Can't get parameter 'Format::fvaDirNameYear' from configuration.";
+		return res;
+	}
+	
 	res = cfg.getParamAsString("Format::fvaFileName", fvaFileName);
-	RET_RES_IF_RES_IS_ERROR
+	if ( FVA_NO_ERROR != res )
+	{
+		LOG_CRIT << "FvaFmtContext::fillFmtContextFromCfg: Can't get parameter 'Format::fvaFileName' from configuration.";
+		return res;
+	}
 
 	res = cfg.getParamAsString("Format::exifDateTime", exifDateTime);
-	RET_RES_IF_RES_IS_ERROR
+	if ( FVA_NO_ERROR != res )
+	{
+		LOG_CRIT << "FvaFmtContext::fillFmtContextFromCfg: Can't get parameter 'Format::exifDateTime' from configuration.";
+		return res;
+	}
 
 	res = cfg.getParamAsString("Format::fileName1", fileName1);
-	RET_RES_IF_RES_IS_ERROR
+	if ( FVA_NO_ERROR != res )
+	{
+		LOG_CRIT << "FvaFmtContext::fillFmtContextFromCfg: Can't get parameter 'Format::fileName1' from configuration.";
+		return res;
+	}
 
 	res = cfg.getParamAsString("Format::fileName2", fileName2);
-	RET_RES_IF_RES_IS_ERROR
+	if ( FVA_NO_ERROR != res )
+	{
+		LOG_CRIT << "FvaFmtContext::fillFmtContextFromCfg: Can't get parameter 'Format::fileName2' from configuration.";
+		return res;
+	}
 
 	res = cfg.getParamAsString("Format::riffDateTime1", riffDateTime1);
-	RET_RES_IF_RES_IS_ERROR
+	if ( FVA_NO_ERROR != res )
+	{
+		LOG_CRIT << "FvaFmtContext::fillFmtContextFromCfg: Can't get parameter 'Format::riffDateTime1' from configuration.";
+		return res;
+	}
 
-	return cfg.getParamAsString("Format::riffDateTime2", riffDateTime2);	
+	if ( FVA_NO_ERROR != cfg.getParamAsString("Format::riffDateTime2", riffDateTime2))
+	{
+		LOG_CRIT << "FvaFmtContext::fillFmtContextFromCfg: Can't get parameter 'Format::riffDateTime2' from configuration.";
+		return res;
+	}
+	return FVA_NO_ERROR;
 }
