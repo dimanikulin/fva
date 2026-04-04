6671321a174bfada70d0f46ae6e86b6bb31c11c7
Dmytro Nikulin <dimanikulin@gmail.com>
2026-04-04 11:13:16 +0300

    #188 added to support move from QT to STL

---
+++ src/commonlib/fva_qt_port_2_stl.cpp
@@ -0,0 +1,18 @@
+/*!
+ * \file fva_qt_port_2_stl.cpp
+ * \copyright Copyright 2021 FVA Software. All rights reserved. This file is released under the XXX License.
+ * \author Dima Nikulin.
+ * \version 0.29
+ * \date  2014-2021
+ */
+#include "fva_qt_port_2_stl.h"
+
+#include <algorithm>
+#include <cctype>
+
+// TODO create UT for this function
+std::string toUpper(const std::string& str)  {
+    std::string result = str;
+    std::transform(result.begin(), result.end(), result.begin(), [](unsigned char c) { return std::toupper(c); });
+    return result;
+}
---
+++ src/commonlib/fva_qt_port_2_stl.h
@@ -0,0 +1,21 @@
+#ifndef FVA_QT_PORT_2_STL_H
+#define FVA_QT_PORT_2_STL_H
+
+/*!
+ * \file fva_qt_port_2_stl.h
+ * \copyright Copyright 2021 FVA Software. All rights reserved. This file is released under the XXX License.
+ * \author Dima Nikulin.
+ * \version 0.29
+ * \date  2014-2021
+ */
+
+#include <string>
+
+/*!
+ * \brief Helper function to convert string to uppercase
+ * \param str The input string
+ * \return The uppercase version of the input string
+ */
+std::string toUpper(const std::string& str);
+
+#endif  // FVA_QT_PORT_2_STL_H
