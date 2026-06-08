c79a80215c9c93921c9f44443d5b26fbe1841e40
Dmytro Nikulin <dimanikulin@gmail.com>
2026-06-08 11:04:58 +0300

    #188 fixing linkage

--- tests/unit/logging_impl.cpp
+++ tests/unit/logging_impl.cpp
@@ -2,4 +2,14 @@
 
 LOGGER_GLOBAL
 
-LOG_INIT("#logs#/tests.txt")
+int main() {
+    FvaConfiguration cfg;
+    const std::string cfgPath = (appDir / "fvaParams.csv").string();
+    FVA_EXIT_CODE exitCode = cfg.load(cfgPath);
+    if (exitCode != FVA_NO_ERROR) {
+        return exitCode;
+    }
+
+    LOG_INIT("#logs#/tests.txt")
+    return 0;
+}
