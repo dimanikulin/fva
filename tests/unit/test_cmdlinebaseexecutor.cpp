9e9903e64eaa766af663e49125181cc1edeb6779
Dmytro Nikulin <dimanikulin@gmail.com>
2026-06-29 08:54:57 +0300

    #188 fixing link error

--- tests/unit/test_cmdlinebaseexecutor.cpp
+++ tests/unit/test_cmdlinebaseexecutor.cpp
@@ -9,6 +9,9 @@
 LOGGER_GLOBAL
 #endif  // LOGGER_GLOBAL
 
+std::ofstream g_logfile;
+int g_logLevel = 2;
+
 // Test fixture for CmdLineBaseExecutor tests
 class CmdLineBaseExecutorTests : public ::testing::Test {
 protected:
